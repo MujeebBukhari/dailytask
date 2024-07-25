676774930b8688558ed48b3ea70e73ed7f0b1b7a
Mujeeb Bukhari <mujeebsyed081@gmail.com>
2024-07-25 21:31:46 +0500

    job added

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\JobPosted;
 use Illuminate\Http\Request;
 use App\Models\Job;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Mail;
 
 class JobController extends Controller
 {
@@ -42,11 +44,17 @@ class JobController extends Controller
             ]);
 
 
-            Job::create([
+            $job = Job::create([
                 'title' => request('title'),
                 'salary' => request('salary'),
                 'employer_id' => 1, 
             ]);
+            
+            
+            Mail::to($job->employer->user)->send(
+                new JobPosted($job)
+            );
+
             return redirect('jobs');
 
     }
